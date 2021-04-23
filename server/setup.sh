a0ee031d3ec009611d56f3579c5a85b1c6d816d4
Josh <lowranceworks@gmail.com>
2021-04-23 10:08:55 -0500

    update

--- server/setup.sh
+++ server/setup.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+# create 
 sudo useradd --no-create-home prometheus
 sudo mkdir /etc/prometheus
-sudo mkdir /var/lib/prometheus
+sudo mkdir /kvar/lib/prometheus
 
+# install prometheus 
 wget https://github.com/prometheus/prometheus/releases/download/v2.26.0/prometheus-2.26.0.linux-amd64.tar.gz
 tar xvfz prometheus-2.26.0.linux-amd64.tar.gz
-
 sudo cp prometheus-2.26.0.linux-amd64/prometheus /usr/local/bin
 sudo cp prometheus-2.26.0.linux-amd64/promtool /usr/local/bin/
 sudo cp -r prometheus-2.26.0.linux-amd64/consoles /etc/prometheus
 sudo cp -r prometheus-2.26.0.linux-amd64/console_libraries /etc/prometheus
-
 sudo cp prometheus-2.26.0.linux-amd64/promtool /usr/local/bin/
 rm -rf prometheus-2.26.0.linux-amd64.tar.gz prometheus-2.26.0.linux-amd64
 
+# add prometheus.yml and define prometheus as a service
 sudo cp ./prometheus.yml /etc/prometheus/prometheus.yml
 sudo cp ./prometheus.service /etc/systemd/system/prometheus.service
 
+# configure permissions 
 sudo chown prometheus:prometheus /etc/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
 sudo chown -R prometheus:prometheus /etc/prometheus/consoles
 sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
 sudo chown -R prometheus:prometheus /var/lib/prometheus
+
+# configure systemd
 sudo systemctl daemon-reload
 sudo systemctl enable prometheus
---
+++ target/setup.sh
@@ -0,0 +1,21 @@
+# create user for prometheus node exporter
+sudo useradd --no-create-home node_exporter
+
+# install binaries
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+tar xzf node_exporter-1.1.2.linux-amd64.tar.gz
+sudo cp node_exporter-1.1.2.linux-amd64/node_exporter /usr/local/bin/node_exporter
+rm -rf node_exporter-1.1.2.linux-amd64.tar.gz node_exporter-1.1.2.linux-amd64
+
+# configure service
+sudo cp ./node-exporter.service /etc/systemd/system/
+
+# configure systemd
+sudo systemctl daemon-reload
+sudo systemctl enable node-exporter
+sudo systemctl start node-exporter
+sudo systemctl status node-exporter
+
+# prompt next steps 
+echo ssh into the prometheus server and add this target to /etc/prometheus/prometheus.yml
+echo restart prometheus after applying these changes 
